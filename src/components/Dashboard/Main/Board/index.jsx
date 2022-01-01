a7ad266d5273a2788ee672dc4f2ff8301c889060
Giancarlo Guerra Farfan <gian.guerra23@gmail.com>
2021-12-31 19:09:10 -0500

    feat(board): Cards ordered in columns by status

--- src/components/Dashboard/Main/Board/index.jsx
+++ src/components/Dashboard/Main/Board/index.jsx
@@ -5,31 +5,62 @@ import { getAllAction } from "../../../../store/features/notes/notesSlice";
 import { useDispatch, useSelector } from "react-redux";
 // import noteService from "../../../../services/notesService";
 function Board() {
-  let notesState = useSelector((state) => state.notes.notes);
   const dispatch = useDispatch();
-  console.log(notesState);
+  let allNotes = useSelector((state) => state.notes.notes);
   useEffect(() => {
     dispatch(getAllAction());
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className={Styles.board_container}>
       <div className={`${Styles.created} ${Styles.column}`}>
-        {notesState.map((note) => {
-          return (
-            <NoteCard
-            key={note.id}
-              isImportant={note.isImportant}
-              comment={note.comment}
-              title={note.title}
-              status={note.status}
-              date={note.date}
-            />
-          );
+        {allNotes.map((note) => {
+          if (note.status === "created") {
+            return (
+              <NoteCard
+                key={note.id}
+                isImportant={note.isImportant}
+                comment={note.comment}
+                title={note.title}
+                status={note.status}
+                date={note.date}
+              />
+            );
+          }
         })}
       </div>
-      <div className={`${Styles.progress} ${Styles.column}`}></div>
-      <div className={`${Styles.completed} ${Styles.column}`}></div>
+      <div className={`${Styles.progress} ${Styles.column}`}>
+        {allNotes.map((note) => {
+          if (note.status === "in_progress") {
+            return (
+              <NoteCard
+                key={note.id}
+                isImportant={note.isImportant}
+                comment={note.comment}
+                title={note.title}
+                status={note.status}
+                date={note.date}
+              />
+            );
+          }
+        })}
+      </div>
+      <div className={`${Styles.completed} ${Styles.column}`}>
+        {allNotes.map((note) => {
+          if (note.status === "completed") {
+            return (
+              <NoteCard
+                key={note.id}
+                isImportant={note.isImportant}
+                comment={note.comment}
+                title={note.title}
+                status={note.status}
+                date={note.date}
+              />
+            );
+          }
+        })}
+      </div>
     </div>
   );
 }
--- src/store/features/notes/notesSlice.js
+++ src/store/features/notes/notesSlice.js
@@ -5,30 +5,20 @@ import noteService from '../../../services/notesService';
 const initialNotes = {
     notes: [],
     loading: false,
-    error: false
+    error: ""
 };
 
 //NOTES REDUCER
 const notesReducer = (state = initialNotes, action) => {
     switch (action.type) {
-        case 'NOTES/GET_ALL_NOTES': {
-            return { ...state, notes: action.payload };
-        }
-        case 'NOTES/ADD_NOTE': {
-            return { ...state, notes: [...state.notes, action.payload] };
-        }
-
-        case 'NOTES/DELETE_NOTE': {
-            return { ...state, notes: [...state.notes].filter(note => note.id !== action.payload) };
-        }
         case 'NOTES/FETCH_REQUEST': {
             return { ...state, loading: true };
         }
         case 'NOTES/FETCH_ERROR': {
-            return { ...state, loading: false, error: true };
+            return { ...state, loading: false, error: action.payload };
         }
         case 'NOTES/FETCH_SUCCESS': {
-            return { ...state, loading: false, error: false };
+            return { ...state, notes: action.payload , loading: false, error: "" };
         }
         default: {
             return state;
@@ -39,11 +29,25 @@ const notesReducer = (state = initialNotes, action) => {
 export const getAllAction = () => {
     return async (dispatch, getState) => {
         const payload = await noteService.getAll();
-        dispatch({ type: 'NOTES/GET_ALL_NOTES', payload: payload });
+        dispatch({ type: 'NOTES/FETCH_SUCCESS', payload: payload });
     }
 }
 
-// export const fetchUserById = createAsyncThunk(
+export const createAction = () => {
+    return async (dispatch, getState) => {
+        const payload = await noteService.create();
+        dispatch({ type: 'NOTES/FETCH_SUCCESS', payload: payload });
+    }
+}
+
+export const updateAction = () => {
+    return async (dispatch, getState) => {
+        const payload = await noteService.update();
+        dispatch({ type: 'NOTES/FETCH_SUCCESS', payload: payload });
+    }
+}
+
+// export const getAllAction = createAsyncThunk(
 //     'NOTES/GET_ALL_NOTES', // action type
 //     async (arg, thunkAPI) => {// payload creator
 //         const response = await noteService.getAll();
