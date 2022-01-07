eb1f90cae3fe1994cc757c7de07eb4a88d52cb27
Giancarlo Guerra Farfan <gian.guerra23@gmail.com>
2022-01-07 17:25:46 -0500

    fix(fn): credentials in register to backend

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,46 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import "./styles.css";
 import Button from "../Button";
-import SignupForm from '../forms/SignupForm/index';
-import SigninForm from '../forms/SigninForm/index';
+import SignupForm from "../forms/SignupForm/index";
+import SigninForm from "../forms/SigninForm/index";
 function Welcome() {
-  
   const [showSignIn, setShowSignIn] = useState(true);
   // const [toCleanSignin, setToCleanSignin] = useState(false);
   // const [toCleanSignup, setToCleanSignup] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   useEffect(() => {
-    if (window.localStorage.getItem("loggedNoteappUser")) {
-      const local_user_data = JSON.parse(window.localStorage.getItem("loggedNoteappUser"));
-      dispatch({ type: "USER/SET_USER", payload: local_user_data });
+    const localUser = window.localStorage.getItem("loggedNoteappUser");
+
+    if (localUser) {
+      const user = JSON.parse(localUser);
+      dispatch({ type: "USER/SET_USER", payload: user });
       navigate("/dashboard");
     }
   }, [dispatch, navigate]);
 
-
   const rightPanel_className = showSignIn ? "" : "right-panel-active";
   const onSignIn = () => {
-    console.log("Sign In");
-    // setToCleanSignin(true);
-    // setToCleanSignup(false);
     setShowSignIn(true);
   };
   const onSignUp = () => {
-    console.log("Sign Up");
-    // setToCleanSignup(true);
-    // setToCleanSignin(false);
     setShowSignIn(false);
   };
   return (
     <main id="welcome" className="welcome">
       <div className={`container ${rightPanel_className}`}>
         {/* <!--  Sign Up  --> */}
-        <SignupForm showSignIn={showSignIn}/>
+        <SignupForm showSignIn={showSignIn} />
         {/* <!-- Sign In  --> */}
-        <SigninForm showSignIn={showSignIn}/>
+        <SigninForm showSignIn={showSignIn} />
         {/* <!-- Overlay   --> */}
         <div className="container__overlay">
           <div className="overlay">
